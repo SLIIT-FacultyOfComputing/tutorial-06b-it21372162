b9500e5f734fb10d47de1eedff8ee91db91825ca
dilshan <99360801+it21372162@users.noreply.github.com>
2022-03-18 14:17:28 +0000

    complete student.h

--- Student.h
+++ Student.h
@@ -1,9 +1,15 @@
 class Student {
   // private section
+private:
+    int studentID ;
+    char studentName[20] ;
   //    int studentId
   //    name <- 20 charcters
   
   // public section
+public :
+    void assignDetails(int no , char name[]) ;
+    void display() ;
   //    assignDetails() method declaration
   //    display() method declaration
 };
